74330f0642c20cb7aaa54b37afd561af499141fd
joshua <joshua@atriomx.ai>
2021-12-15 10:27:37 -0600

    Adding profile and construction message

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Under construction.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img style={{width: 120, height: 120, borderRadius: 120/2}} src="./profile.jpeg" alt="profile_picture" />
+        <Typography style={{fontSize: 30}}>
+          Joshua D'Arcy, M.D.
+        </Typography>
+        <Typography style={{fontSize: 16, align:"center", color: "lightgrey"}}>
+          Machine Learning Engineer
+        </Typography>
+        <Typography style={{fontSize: 16, align:"center", color: "lightgrey"}}>
+          Full Stack Developer
+        </Typography>
+        <Typography style={{fontSize: 8, align:"center", color: "lightgrey"}}>
+          <i>(Resume website under development: 12.15.2021)</i>
+        </Typography>
       </header>
     </div>
   );
