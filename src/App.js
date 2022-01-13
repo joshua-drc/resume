b2e402adb09e12e7cdffbe49fa6a4231e5bc6e6f
Joshua D'Arcy <36454942+joshua-drc@users.noreply.github.com>
2022-01-13 10:06:33 -0600

    Updating development statement

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           Full Stack Developer
         </Typography>
         <Typography style={{fontSize: 8, align:"center", color: "lightgrey"}}>
-          <i>(Resume website under development: 12.15.2021)</i>
+          <i>(Resume website under development)</i>
         </Typography>
       </header>
     </div>
